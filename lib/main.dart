fe1720fc095eebd6814063f66e70bdc3bc6e8470
ShRudra88 <salhawathosenrudra503@gmail.com>
2023-09-29 23:20:48 +0600

    Question
    Create a dummy image feed that adjusts its layout based on the device's orientation.
    
    The feed should display multiple thumbnail images in list format for portrait and grid format in landscape.
    
    Use a Card or Container type widget to hold the thumbnail image (Can use image/images of your choice, they will be 150x150 in size or use this image -> “https://via.placeholder.com/150”)
    
    Show at least 10 items on screen.
    
    On GridView the crossAxis count will be minimum 2.

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,37 @@
 import 'package:flutter/material.dart';
 
-// s_h_rudra88
+void main() => runApp(ImageFeedApp());
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
+class ImageFeedApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CounterScreen(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Image Feed'),
+        ),
+        body: ImageFeed(),
+      ),
     );
   }
 }
 
-class CounterScreen extends StatefulWidget {
-  @override
-  _CounterScreenState createState() => _CounterScreenState();
-}
-
-class _CounterScreenState extends State<CounterScreen> {
-  int count = 0;
-
-  void incrementCount() {
-    setState(() {
-      count++;
-      if (count >= 5) {
-        showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              title: Text("Button pressed $count times."),
-              actions: <Widget>[
-                TextButton(
-                  child: Text("OK"),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ],
-            );
-          },
-        );
-      }
-    });
-  }
-
-  void decrementCount() {
-    setState(() {
-      if (count > 0) {
-        count--;
-      }
-    });
-  }
+class ImageFeed extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Counter App'),
+    final orientation = MediaQuery.of(context).orientation;
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: orientation == Orientation.portrait ? 1 : 2,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Count: $count',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                FloatingActionButton(
-                  onPressed: incrementCount,
-                  child: Icon(Icons.add),
-                ),
-                SizedBox(width: 20),
-                FloatingActionButton(
-                  onPressed: decrementCount,
-                  child: Icon(Icons.remove),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Card(
+          child: Image.network("https://via.placeholder.com/150"),
+        );
+      },
     );
   }
 }
