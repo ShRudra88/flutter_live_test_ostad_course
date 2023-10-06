c8c0c7e8b176c45eecdd8d9492d1ecdb9908a245
ShRudra88 <salhawathosenrudra503@gmail.com>
2023-10-06 23:44:01 +0600

    Design the following screen. On tapping on a button the colour of that button will be changed and reset the previous selected one. Also show a Snackbar on each button press with the selected Size text.

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,146 @@
 import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
 
-void main() => runApp(ImageFeedApp());
-
-class ImageFeedApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     // debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          //centerTitle: true,
-          title: Text('Image Feed'),
-        ),
-        body: ImageFeed(),
-      ),
+      home: MyHomePage(),
     );
   }
 }
 
-class ImageFeed extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int selectedButtonIndex = -1;
+
+  void _showSnackBar(String label) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Size $label selected'),
+        duration: Duration(seconds: 2),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    final orientation = MediaQuery.of(context).orientation;
-
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: orientation == Orientation.portrait ? 1 : 2,
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Size Selector'),
       ),
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return Card(
-          child: Image.network("https://via.placeholder.com/150"),
-        );
-      },
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedButtonIndex = 0;
+                      _showSnackBar('S');
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary:
+                    selectedButtonIndex == 0 ? Colors.yellow : null,
+                  ),
+                  child: Text('S'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedButtonIndex = 1;
+                      _showSnackBar('M');
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary:
+                    selectedButtonIndex == 1 ? Colors.yellow : null,
+                  ),
+                  child: Text('M'),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedButtonIndex = 2;
+                      _showSnackBar('L');
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary:
+                    selectedButtonIndex == 2 ? Colors.yellow : null,
+                  ),
+                  child: Text('L'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedButtonIndex = 3;
+                      _showSnackBar('XL');
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary:
+                    selectedButtonIndex == 3 ? Colors.yellow : null,
+                  ),
+                  child: Text('XL'),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedButtonIndex = 4;
+                      _showSnackBar('XXL');
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary:
+                    selectedButtonIndex == 4 ? Colors.yellow : null,
+                  ),
+                  child: Text('XXL'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedButtonIndex = 5;
+                      _showSnackBar('XXXL');
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary:
+                    selectedButtonIndex == 5 ? Colors.yellow : null,
+                  ),
+                  child: Text('XXXL'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
